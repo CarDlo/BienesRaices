7dd82358ab27096fc6f91abab131d6443a323acb
CarDloDev <charles4586@gmail.com>
2024-08-03 12:22:57 -0500

    Creando header con PHP

--- index.php
+++ index.php
@@ -1,33 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="build/css/app.css">
-    <title>Bienes Raices</title>
-</head>
-<body>
-    
-    <header class="header inicio">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="/"><img src="build/img/logo.svg" alt="Bienes Raices"> </a>
-                <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu">
-                </div>
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="Dark mode">
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-            </div>
-            <h1>Venta de Casas y Departamentos</h1>
-        </div>
-    </header>
+<?php
+    $inicio = true;
+    include './includes/templates/header.php';
+?>
     <main class="contenedor">
         <h1>Mas sobre nosotros</h1>
 
--- nosotros.php
+++ nosotros.php
@@ -1,34 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="build/css/app.css">
-    <title>Nosotros</title>
-</head>
-<body>
-    
-    <header class="header">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="/"><img src="build/img/logo.svg" alt="Bienes Raices"> </a>
-
-                <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu">
-                </div>
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="Dark mode">
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-
-            </div>
-        </div>
-    </header>
+<?php
+    $inicio = false;
+    include './includes/templates/header.php';
+?>
     <main class="contenedor">
         <h1>Conoce sobre nosotros</h1>
         <div class="contenido-nosotros">
