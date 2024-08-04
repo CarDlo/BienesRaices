6b8455eab241cec0faa243592245609c0639200a
CarDloDev <charles4586@gmail.com>
2024-08-04 10:04:33 -0500

    Crear un select con lavores de la base de datos

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,13 @@
     //Arreglo con mensajes de error
     $errores = [];
 
+    $titulo = '';
+    $precio = '';
+    $descripcion = '';
+    $habitaciones = '';
+    $wc = '';
+    $estacionamiento = '';
+    $vendedor = '';
 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $titulo = $_POST['titulo'];
@@ -70,27 +77,27 @@
         <fieldset>
             <legend>Informacion General</legend>
             <label for="titulo">Título</label>
-            <input type="text" placeholder="Título Propiedad" id="titulo" name="titulo">
+            <input type="text" placeholder="Título Propiedad" id="titulo" name="titulo" value="<?php echo $titulo; ?>">
 
             <label for="precio">Precio</label>
-            <input type="number" placeholder="Precio Propiedad" id="precio" name="precio">
+            <input type="number" placeholder="Precio Propiedad" id="precio" name="precio" value="<?php echo $precio; ?>">
 
             <label for="imagen">Imagen</label>
             <input type="file" id="imagen" accept="image/jpeg, image/png">
 
             <label for="descripcion">Descripción</label>
-            <textarea id="descripcion" name="descripcion"></textarea>
+            <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
         </fieldset>
         <fieldset>
             <legend>Información de la Propiedad</legend>
             <label for="habitaciones">Habitaciones</label>
-            <input type="number" placeholder="Ej: 3" id="habitaciones" min="1" max="9" name="habitaciones">
+            <input type="number" placeholder="Ej: 3" id="habitaciones" min="1" max="9" name="habitaciones" value="<?php echo $habitaciones; ?>">
 
             <label for="wc">Baños</label>
-            <input type="number" placeholder="Ej: 3" id="wc" min="1" max="9" name="wc">
+            <input type="number" placeholder="Ej: 3" id="wc" min="1" max="9" name="wc" value="<?php echo $wc; ?>">
 
             <label for="estacionamiento">Estacionamiento</label>
-            <input type="number" placeholder="Ej: 3" id="estacionamiento" min="1" max="9" name="estacionamiento">
+            <input type="number" placeholder="Ej: 3" id="estacionamiento" min="1" max="9" name="estacionamiento" value="<?php echo $estacionamiento; ?>">
         </fieldset>
         <fieldset>
             <legend>Vendedor</legend>
