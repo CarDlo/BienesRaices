5933fef4a8316aa0f93d636169eacc353e2d687f
CarDloDev <charles4586@gmail.com>
2024-08-03 21:28:15 -0500

    HTML y el formulario para crear propiedad

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,41 @@
 <main class="contenedor seccion"> 
     <h1>Crear propiedad</h1>
     <a href="/BienesRaices/admin/index.php"><input type="submit" value="Volver" class="boton-verde"></a>
+    <form class="formulario">
+        <fieldset>
+            <legend>Informacion General</legend>
+            <label for="titulo">Título</label>
+            <input type="text" placeholder="Título Propiedad" id="titulo">
+
+            <label for="precio">Precio</label>
+            <input type="number" placeholder="Precio Propiedad" id="precio">
+
+            <label for="imagen">Imagen</label>
+            <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+            <label for="descripcion">Descripción</label>
+            <textarea id="descripcion"></textarea>
+        </fieldset>
+        <fieldset>
+            <legend>Información de la Propiedad</legend>
+            <label for="habitaciones">Habitaciones</label>
+            <input type="number" placeholder="Ej: 3" id="habitaciones" min="1" max="9">
+
+            <label for="wc">Baños</label>
+            <input type="number" placeholder="Ej: 3" id="wc" min="1" max="9">
+
+            <label for="estacionamiento">Estacionamiento</label>
+            <input type="number" placeholder="Ej: 3" id="estacionamiento" min="1" max="9">
+        </fieldset>
+        <fieldset>
+            <legend>Vendedor</legend>
+            <label for="vendedor">Vendedor</label>
+            <select name="vendedores" id="vendedor">
+                <option value="">-- Seleccione --</option>
+            </select>
+        </fieldset>
+        <input type="submit" value="Crear Propiedad" class="boton-verde">
+    </form>
 </main>
 <?php
     incluirTemplate('footer');
