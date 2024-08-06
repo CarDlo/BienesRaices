6be772c71198038d925316d9fcd11e7647cf1b11
CarDloDev <charles4586@gmail.com>
2024-08-05 21:48:16 -0500

    Creando una tabla para la base de datos

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,33 @@
     <?php } ?>
 
     <a href="propiedades/crear.php"><input type="submit" value="Nueva Propiedad" class="boton-verde"></a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>1</th>
+                <th>Titulo</th>
+                <th><img src="/BienesRaices/imagenes/9459b34cbf8166b040e680139acdf8f4.jpg" class="imagen-tabla"></th>
+                <th>10000</th>
+                <th>
+                    <a href="propiedades/actualizar.php?id=1" class="boton-verde-block">Actualizar</a>
+                    <a href="propiedades/eliminar.php?id=1" class="boton-amarillo-block">Eliminar</a>
+                </th>
+            </tr>
+        </tbody>
+    </table>
+
+
+
 </main>
 <?php
     incluirTemplate('footer');
