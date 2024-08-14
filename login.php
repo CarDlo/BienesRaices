e71b823fc6b201da227dc0463366ece4c7eebe59
CarDloDev <charles4586@gmail.com>
2024-08-14 18:03:57 -0500

    Validacion de formulario

--- login.php
+++ login.php
@@ -1,19 +1,46 @@
 <?php
+
+require 'includes/config/database.php';
+$db = conectarDB();
+
+$errores=[];
+//autenticar el usuario
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+//Sanitizando los datos
+    $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if(!$email) {
+        $errores[] = "El email es obligatorio o no es válido";
+    }
+
+    if(!$password) {
+        $errores[] = "El password es obligatorio";
+    }
+
+}
+
+//incluye header
     require 'includes/funciones.php';
- 
     incluirTemplate('header');
 ?>
 
 <main class="contenedor seccion contenido-centrado"> 
     <h1>Iniciar Sesión</h1>
-    <form class="formulario" method="POST" action="">
+    <?php foreach($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php endforeach; ?>
+
+    <form class="formulario" method="POST" action="" novalidate>
     <fieldset>
                 <legend>Email y password</legend>
 
                 <label for="email">E-mail</label>
-                <input type="email" placeholder="Tu E-mail" id="email">
+                <input name="email" type="email" placeholder="Tu E-mail" id="email" required>
                 <label for="password">Password</label>
-                <input type="password" placeholder="Tu Password" id="password">
+                <input name="password" type="password" placeholder="Tu Password" id="password" required>
             </fieldset>
             <input type="submit" value="Iniciar Sesión" class="boton boton-verde">
     </form>
