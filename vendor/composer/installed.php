a50a5bfcfc8aa49db56a9e337fa05bc49dd75d3a
CarDloDev <charles4586@gmail.com>
2024-08-18 22:51:28 -0500

    Instalacion de composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cardlo/bienesraices',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8cfed6ea53df7d57ead5058c2abc5b2d96f495cd',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cardlo/bienesraices' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8cfed6ea53df7d57ead5058c2abc5b2d96f495cd',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
